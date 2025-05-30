5270141b11d7ce7cf5ab3942ac0cfdbc262b99a2
taimoorpk <kh.taimoor@gmail.com>
2025-05-30 15:58:06 +0100

    implemnted lambda to extract secrets

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,13 @@
 resource "aws_lambda_function" "ingestion_lambda" {
 
   function_name = "ingestion_lambda"
-  filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
-
+#   filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
+  s3_bucket = "code-bucket-totesys-project"
+  s3_key =  "function.zip"
   role    = aws_iam_role.ingestion_lambda_role.arn
-  handler = "test_lambda_handler.lambda_handler"
+  handler = "secrets.lambda_handler"
+  layers = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
+  
   runtime = "python3.9"
 
   environment {
@@ -14,11 +17,11 @@ resource "aws_lambda_function" "ingestion_lambda" {
   }
 }
 
-data "archive_file" "ingestion_zip" { ## Zipping ingestion lambda function
-  type        = "zip"
-  source_file = "${path.module}/../terraform/terraform_test/test_lambda_handler.py" ##local file path to be zipped
-  output_path = "${path.module}/../terraform/terraform_test/ingestion_function.zip" ## where zipped function will be stored
-}
+# data "archive_file" "ingestion_zip" { ## Zipping ingestion lambda function
+#   type        = "zip"
+#   source_file = "${path.module}/../terraform/terraform_test/test_lambda_handler.py" ##local file path to be zipped
+#   output_path = "${path.module}/../terraform/terraform_test/ingestion_function.zip" ## where zipped function will be stored
+# }
 
 
 
