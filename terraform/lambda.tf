b42df796ec0a672a38c0f701894604060f54b439
taimoorpk <kh.taimoor@gmail.com>
2025-05-30 16:00:01 +0100

    formatted implemnted lambda to extract secrets

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,13 @@
 resource "aws_lambda_function" "ingestion_lambda" {
 
   function_name = "ingestion_lambda"
-#   filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
+  #   filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
   s3_bucket = "code-bucket-totesys-project"
-  s3_key =  "function.zip"
-  role    = aws_iam_role.ingestion_lambda_role.arn
-  handler = "secrets.lambda_handler"
-  layers = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
-  
+  s3_key    = "function.zip"
+  role      = aws_iam_role.ingestion_lambda_role.arn
+  handler   = "secrets.lambda_handler"
+  layers    = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
+
   runtime = "python3.9"
 
   environment {
