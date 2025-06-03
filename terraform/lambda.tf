304bf1d0c0383f3a15a7363dd82f2b00c45d379e
Marc-S719 <marcabi@hotmail.fr>
2025-06-03 14:30:49 +0100

    Update lambda.tf
    changed handler filename

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   s3_bucket = "code-bucket-totesys-project"
   s3_key    = "lambda-funtion.zip"
   role      = aws_iam_role.ingestion_lambda_role.arn
-  handler   = "secrets.lambda_handler"
+  handler   = "lambda_for_secrets.lambda_handler"
   layers    = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
 
   runtime = "python3.9"
