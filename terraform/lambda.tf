9d63c5e6ac54c9fc7914c335c511c652bbb34f22
nahisah <nahisahnasleem@gmail.com>
2025-06-03 14:23:56 +0100

    comments removed

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,6 @@
 resource "aws_lambda_function" "ingestion_lambda" {
 
   function_name = "ingestion_lambda"
-  #   filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
   s3_bucket = "code-bucket-totesys-project"
   s3_key    = "lambda-funtion.zip"
   role      = aws_iam_role.ingestion_lambda_role.arn
@@ -12,16 +11,12 @@ resource "aws_lambda_function" "ingestion_lambda" {
 
   environment {
     variables = {
-      BUCKET_NAME = aws_s3_bucket.ingestion-bucket.bucket ##referenced as os.environ["BUCKET_NAME"] in python
+      BUCKET_NAME = aws_s3_bucket.ingestion-bucket.bucket 
     }
   }
 }
 
-# data "archive_file" "ingestion_zip" { ## Zipping ingestion lambda function
-#   type        = "zip"
-#   source_file = "${path.module}/../terraform/terraform_test/test_lambda_handler.py" ##local file path to be zipped
-#   output_path = "${path.module}/../terraform/terraform_test/ingestion_function.zip" ## where zipped function will be stored
-# }
+
 
 
 
