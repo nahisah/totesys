5d65ea1cdc2ffa33b7bc755544c3db7c96121c0a
mkrighthere <martakaczan91@gmail.com>
2025-06-04 11:12:56 +0100

    change bucket to id in the output

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "lambda_function_name" {
   description = "The deployed Lambda function name"
   value       = length(aws_lambda_function.ingestion_lambda) > 0 ? aws_lambda_function.ingestion_lambda[0].function_name : ""
 }
+
+
+output "code_bucket_name" {
+value = aws_s3_bucket.data_bucket.id
+}
