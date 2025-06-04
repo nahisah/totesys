825565d6bfe3f18a54c99c670aa65022ca1039ec
Marc-S719 <marcabi@hotmail.fr>
2025-06-04 14:55:37 +0100

    Fixed up lambda handler and workflow for zipping up lambdas

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
 
   function_name = "ingestion_lambda"
   s3_bucket     = "code-bucket-totesys-project"
-  s3_key        = "lambda-function.zip"
+  s3_key        = "ingestion.zip"
   role          = aws_iam_role.ingestion_lambda_role.arn
   handler       = "lambda_for_secrets.lambda_handler"
   layers        = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
