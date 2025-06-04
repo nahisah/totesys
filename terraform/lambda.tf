125b6a9d1909390f9f3ee91a30b6d1f0ca1a72cf
mkrighthere <martakaczan91@gmail.com>
2025-06-04 15:17:43 +0100

    github actions changes

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   function_name = "ingestion_lambda"
   #   filename      = data.archive_file.ingestion_zip.output_path ## reference to zipped ingestion lambda
   s3_bucket = aws_s3_bucket.code-bucket.bucket
-  s3_key    = "lambda-function.zip"
+  s3_key    = "ingestion.zip"
   role      = aws_iam_role.ingestion_lambda_role.arn
   handler   = "secrets.lambda_handler"
   layers    = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
