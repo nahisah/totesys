5aa3548ff550f21396c465d504d3b36d9f545c4b
Marc-S719 <marcabi@hotmail.fr>
2025-06-04 15:36:24 +0100

    Changed lambda folder structure

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,6 +6,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   role          = aws_iam_role.ingestion_lambda_role.arn
   handler       = "lambda_for_secrets.lambda_handler"
   layers        = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
+  timeout       = 20
 
   runtime = "python3.9"
 
