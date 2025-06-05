48790e1abfbc68d82600ac660ddc880aae3b8ea2
taimoorpk <kh.taimoor@gmail.com>
2025-06-05 17:47:28 +0300

    checked for notification

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   s3_bucket = aws_s3_bucket.code-bucket.bucket
   s3_key    = "ingestion.zip"
   role      = aws_iam_role.ingestion_lambda_role.arn
-  handler   = "secrets.lambda_handler"
+  handler   = "lambda_for_secrets.lambda_handler"
   layers    = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:17"]
   timeout   = 20
   runtime   = "python3.9"
