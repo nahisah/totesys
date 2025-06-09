e8a5630ff42d6ee1a2d1d457d2dde6500945d40e
nahisah <nahisahnasleem@gmail.com>
2025-06-09 16:40:09 +0100

    starting load utils

---
+++ src/load/load_utils.py
@@ -0,0 +1,18 @@
+import awswrangler as wr
+import pandas as pd
+import os
+from datetime import datetime
+
+os.environ["TRANSFORM_BUCKET_NAME"] = "processed-bucket"
+def get_parquet_data_from_processed_bucket(table_name):
+
+    s3_path = f"s3://{os.environ["TRANSFORM_BUCKET_NAME"]}/{table_name}/"
+
+    try:
+        df = wr.s3.read_parquet(path=s3_path)
+        print (df)
+    except Exception as e:
+        raise RuntimeError(f"Error reading parquet from S3 for {table_name}: {e}")
+    
+
+print(get_parquet_data_from_processed_bucket("dim_design"))
